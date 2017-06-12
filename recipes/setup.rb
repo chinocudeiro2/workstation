0f206a3d9a703b269950e5427e752d7177b5b80f
Luis Muñiz <lmuniz@pue.es>
2017-06-12 17:43:05 +0200

    Version 0.2.0: /etc/motd también muestra la IP, el hostname, la memoria y la velocidad de la CPU

--- recipes/setup.rb
+++ recipes/setup.rb
@@ -1,6 +1,15 @@
 package "tree"
 
 file "/etc/motd" do
-  content "Property of Luis\n"
+  content "Property of Luis
+  IPADDRESS: #{node['ipaddress']}
+  HOSTNAME:  #{node['hostname']}
+  MEMORY:    #{node['memory']['total']}
+  CPU:       #{node['cpu']['0']['mhz']}
+"
+
+  mode 0644
+  owner 'root'
+  group 'root'
 end
 
